3888e93a7ff79bbf6e527709e8ec857889cf59f7
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2019-12-08 13:34:53 +0300

    cpu mesh

--- common/src/mesh.rs
+++ common/src/mesh.rs
@@ -1,1 +1,11 @@
+use nalgebra::{Vector2, Vector3, Vector4};
 
+pub struct Mesh {
+    pub indices: Vec<u32>,
+    pub positions: Vec<Vector3<f32>>,
+    pub normals: Vec<Vector3<f32>>,
+    pub tangents: Vec<Vector3<f32>>,
+    pub bitangents: Vec<Vector3<f32>>,
+    pub colors: [Vec<Vector4<f32>>; 4],
+    pub uvs: [Vec<Vector2<f32>>; 8],
+}
